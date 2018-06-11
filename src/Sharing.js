a2fe91fd0bc3e1fe3ba7bad951a2100ff98aaffb
PeterGawecki <pgawecki@hotmail.com>
2018-06-11 18:56:50 -0400

    attempt at text box displaying for comments

--- src/Sharing.js
+++ src/Sharing.js
@@ -3,8 +3,9 @@ import React,{Component} from 'react'
 class Sharing extends Component {
     render(){
         return(
+          <div>
             <div className="article-links">
-          <a className="article-link" href="#">
+          <a className="article-link" href="#" onclick="show()">
             <i className="fa fa-comments-o"></i>
             <span className="article-link-text">Comments</span>
           </a>
@@ -13,7 +14,19 @@ class Sharing extends Component {
             <span className="article-link-text">Share Post</span>
           </a>
         </div>
+        <textarea rows="5" cols="50" id = "text"></textarea>
+        </div>
         )
+    }
+
+    show(){
+      const x = document.getElementById("text")
+      if(x.style.display === "none"){
+        x.style.display = "block"
+      }
+      else{
+        x.style.display = "none"
+      }
     }
 }
 
